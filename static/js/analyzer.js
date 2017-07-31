c2c70df08e0792562956c703b9451067db964e7b
Andrew Hwang <hwandrew@umich.edu>
2017-07-31 18:19:55 -0400

    implemented very basic beat detection

--- static/js/analyzer.js
+++ static/js/analyzer.js
@@ -1,6 +1,11 @@
 // establish all variables that the analyzer will use
 var audio, canvas, ctx, source, context, analyzer, fbcArray,
   bars, barX, barWidth, barHeight;
+var levelsData = [];
+var beatCutOff = 0;
+var beatMin = 0.15;
+var beatHoldTime = 40;
+var beatDecayRate = 0.97;
 
 // initialize analyzer data
 function initAnalyzer() {
@@ -18,22 +23,65 @@ function initAnalyzer() {
   updateFrame();
 }
 
+function updateAnalyzer() {
+  fbcArray = new Uint8Array(analyzer.frequencyBinCount);
+  analyzer.getByteFrequencyData(fbcArray); // length: 1024
+
+  // normalize fbcArray
+  for (var i = 0; i < 32; i++) {
+    var sum = 0;
+    for (var j = 0; j < 32; j++) {
+      sum += fbcArray[(i * 32) + j];
+    }
+    levelsData[i] = sum / 32 / 256;
+  }
+
+  var sum = 0;
+  for (var i = 0; i < 32; i++) {
+    sum += levelsData[i];
+  }
+  level = sum / 32;
+
+  if (level  > beatCutOff && level > beatMin){
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.arc(canvas.width - 20, 20, 20, 0, 2 * Math.PI);
+    ctx.fill();
+    beatCutOff = level *1.1;
+    beatTime = 0;
+  }else{
+    if (beatTime <= beatHoldTime){
+      beatTime ++;
+    }else{
+      beatCutOff *= beatDecayRate;
+      beatCutOff = Math.max(beatCutOff, beatMin);
+    }
+  }
+
+  // set visualizer shapes
+  ctx.fillStyle = '#ffffff';
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, fbcArray[100], 0, 2 * Math.PI);
+  ctx.fill();
+  bars = 100;
+  barWidth = 2;
+  for (var i = 0; i < bars; i++) {
+    barX = i * 10;
+    barHeight = -(fbcArray[i * 5]);
+    ctx.fillRect(barX, canvas.height, barWidth, barHeight);
+  }
+}
+
 function updateFrame() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   window.requestAnimationFrame(updateFrame);
-  fbcArray = new Uint8Array(analyzer.frequencyBinCount);
-  analyzer.getByteFrequencyData(fbcArray); // length of 1024
-  // console.log(fbcArray[1000]);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = '#00CCFF';
-  bars = 200;
-  for (var i = 0; i < bars; i++) {
-    barX = i * 2;
-    barWidth = 1;
-    barHeight = -(fbcArray[i] / 2);
-    ctx.fillRect(barX, canvas.height, barWidth, barHeight);
-  }
+
+  // set background
+  ctx.fillStyle = '#add8e6';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  updateAnalyzer();
 }
 
 $(function() {
